51e0e070814561b8faab93b3404722650af199e6
zhuhaitao <mnizht@qq.com>
2019-08-09 18:37:35 +0800

    列表元组字典集合demo

--- venv/module/demo.py
+++ venv/module/demo.py
@@ -1,7 +1,4 @@
-for n in range(2, 10):
-    for x in range(2, n):
-        if n % x == 0:
-            print(n, 'equals', x, '*', n // x)
-            break
-    else:
-        print(n, 'is a prime number(素数)')
+while True:
+    name = input('enter your name:')
+
+    print(name)
