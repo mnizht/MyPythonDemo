66c1d979031b248cca199627902b66e27f0310a1
zhuht <mnizhuht@gmail.com>
2019-10-22 09:37:30 +0800

    检查是否有真人验证，需人工操作通过

--- venv/src/tianyancha/CompanyName.py
+++ venv/src/tianyancha/CompanyName.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 import xlwings as xw
@@ -10,7 +11,7 @@ from src.autoclick import cookie_data
 def save_evaluate(page, size):
     # 获取公司名称
     try:
-        if page >= 10:
+        if page > 250:
             return 0
         companies = driver.find_elements_by_class_name('sv-search-company')
         curr_size = len(companies)
@@ -19,7 +20,7 @@ def save_evaluate(page, size):
         print(curr_size)
         row = page * size + 1
         for company in companies:
-            name = company.find_element_by_class_name('tyc-event-ch').text
+            name = company.find_element_by_class_name('name').text
             print(name)
             sheet.range('A%d' % row).value = name
             row = row + 1
@@ -49,7 +50,7 @@ sheet = file.sheets[0]
 # 天眼查主页
 tyc = 'https://www.tianyancha.com/'
 driver.get(tyc)
-time.sleep(30)
+time.sleep(20)
 driver.refresh()
 # 输入关键词查询
 key_words = '学生教育培训'
@@ -62,8 +63,10 @@ time.sleep(2)
 # 读取公司名称
 page = 0
 size = 20
+err_num = 0
 while True:
     # 获取下一页按钮
+
     try:
         next_btn = driver.find_element_by_class_name("-next")
         curr_size = save_evaluate(page, size)
@@ -71,12 +74,13 @@ while True:
             break
         page = page + 1
         next_btn.send_keys(Keys.ENTER)
-        time.sleep(1)
+        time.sleep(random.random() * 2 + 1)
     except:
-        print('没有下一页了。。。。。。。')
-        next_btn = None
+        print('检查是否有人工验证')
+        err_num = err_num + 1
+        time.sleep(20)
 
-    if next_btn == None:
+    if err_num > 5:
         save_evaluate(page, size)
         break
 time.sleep(1)
