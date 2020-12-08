de74171aeafec7e31eb66a1c48a3b7a0577b09b1
zhuhaitao <zhuhaitao@email.com>
2020-12-08 11:38:10 +0800

    feat():修改生成数据的方法

--- venv/src/faker/fakerInfo.py
+++ venv/src/faker/fakerInfo.py
@@ -6,10 +6,13 @@ fake = Faker(locale='zh_CN')
 app = xw.App(visible=True, add_book=False)
 file = app.books.add()
 sheet1 = file.sheets[0]
-for num in range(1, 100001):
+for num in range(1, 100000):
     sheet1.range('A%d' % num).value = num
-    sheet1.range('B%d' % num).value = fake.name()
-    sheet1.range('C%d' % num).value = fake.phone_number()
+    sheet1.range('C%d' % num).value = fake.name()
+    sheet1.range('D%d' % num).value = fake.phone_number()
+    sheet1.range('E%d' % num).value = fake.phone_number()
+    sheet1.range('F%d' % num).value = fake.address()
+    sheet1.range('E%d' % num).value = fake.company()
 
-file.save('e://faker.xlsx')
+file.save('d://faker2.xlsx')
 print("End")
